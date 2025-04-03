ad39f3738130b5b37f069fba0c5b063fb4b09160
mSafa <safaktrcgl@gmail.com>
2025-04-04 00:27:39 +0300

    file renaming

--- Assets/Scripts/SomewhatLauncher/ApplicationLauncher.cs
+++ Assets/Scripts/SomewhatLauncher/ApplicationLauncher.cs
@@ -11,7 +11,7 @@ namespace SomewhatLauncher
             foo.transform.SomewhatMove(Vector3.up * 5 + Vector3.right * 3, 2.5f, () => Debug.Log("Foo"));
 
             var bar = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            bar.transform.SomewhatScale(Vector3.zero, 3.5f, () => Debug.Log("Bar"));
+            bar.transform.SomewhatScale(Vector3.one * 2, 3.5f, () => Debug.Log("Bar"));
             
             var baz = GameObject.CreatePrimitive(PrimitiveType.Capsule);
             baz.transform.SomewhatRotate(Vector3.right * 180, 1.5f, () => Debug.Log("Baz"));
